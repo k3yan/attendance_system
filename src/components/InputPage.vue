a6ae8a9b0ffc35c757115b5797bde910e03ab470
Khian Abad <123443469+k3yan@users.noreply.github.com>
2024-04-28 10:39:14 +0800

    table

--- src/components/InputPage.vue
+++ src/components/InputPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-auto h-auto">
+  <div class="w-auto h-screen">
     <img src="../assets/bgbook.jpg" alt="bg-image" class="absolute w-full h-full"/>
       <div class="absolute mt-4 ml-[2rem] flex justify-between gap-3 bg-cover">
         <img src="../assets/emanuscript-logo.png" alt="logo" class="flex h-[7rem] rounded-full" />
@@ -20,17 +20,17 @@
           Recently Confirmed
         </div>
       <div class="flex justify-center mt-2 px-1 py-auto">
-      <table class="border-collapse border-2 border-blue-500 absolute">
+      <table class="border-collapse border-2 border-gray-500 absolute">
         <thead class="">
           <tr class="">
             <th class="border border-gray-500 px-10">Student ID</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="border border-gray-500">Data 1</td>
-          </tr>
-        </tbody>
+      <tr v-for="(row, index) in tableData" :key="index">
+        <td class="px-4 py-2">{{ row.value }}</td>
+      </tr>
+      </tbody>
       </table>
       </div>
       </div>
@@ -38,24 +38,38 @@
   </div>
     <img class="h-[10rem] ml-[7rem] flex" src="../assets/librarylogo.png">
   </div>
+  <form @submit.prevent="addRow">
   <div class="flex flex-col gap-4 p-6">
     <div class="relative h-11 w-full min-w-[200px]">
       <input
-        class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-gray-900 border-t-transparent text-white outline outline-0 placeholder-shown:border placeholder-shown:border-blue-100 placeholder-shown:border-t-blue-100 focus:border-2 focus:border-gray-100 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-50"
-        placeholder=" " v-model="inputValue" />
+        class="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-gray-900 
+        border-t-transparent text-white outline outline-0 placeholder-shown:border placeholder-shown:border-blue-100 placeholder-shown:border-t-blue-100 
+        focus:border-2 focus:border-gray-100 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-50"
+        placeholder=" " v-model="inputValue" type="text" />
       <label
-        class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-200 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-gray-100 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-gray-100 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-blue-200 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-200 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-500">
+        class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none 
+        !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-200 transition-all before:pointer-events-none 
+        before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l
+         before:border-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 
+         after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-gray-100 after:transition-all peer-placeholder-shown:text-sm 
+         peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-gray-100 peer-placeholder-shown:before:border-transparent 
+         peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-white peer-focus:before:border-t-2 
+         peer-focus:before:border-l-2 peer-focus:before:!border-blue-200 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-blue-200 
+         peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-500">
         ID Number:
       </label>
     </div>
   </div>
   <div class="p-6 pt-0">
     <button
-      class="block w-full select-none rounded-lg bg-gradient-to-tr from-red-950 to-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button" @click="logInput">
+      class="block w-full select-none rounded-lg bg-gradient-to-tr from-red-950 to-gray-900 py-3 px-6 text-center 
+      align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg
+       hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      type="submit">
       Confirm
     </button>
   </div>
+</form>
 </div>
   </div>
 </template>
@@ -66,15 +80,19 @@ data() {
     return {
       showTable: false,
       inputValue: '',
+      tableData: [],
     }
   },
   methods: {
     toggleTable() {
       this.showTable = !this.showTable;
     },
-    logInput() {
-      console.log(this.inputValue);
-    },
+    addRow(){
+      if (this.inputValue !== ''){
+        this.tableData.push({value: this.inputValue});
+        this.inputValue = '';
+      }
+    }
   },
 };
 </script>
